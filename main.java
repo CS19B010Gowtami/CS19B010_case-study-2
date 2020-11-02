89cbccb7ca112ccec1b4a7492c6af7902718fd2d
CS19B010Gowtami <70938268+CS19B010Gowtami@users.noreply.github.com>
2020-11-02 14:06:05 +0530

    Update main.java

--- main.java
+++ main.java
@@ -1,1 +1,30 @@
-
+public class CS19B010 
+{
+	public static void main(String[] args) 
+	{
+		boolean isvalid=false;
+        Scanner sc = new Scanner(System.in);
+	    MyClass obj=new MyClass();//object of MyClass
+	    Authentication aut=new Authentication();
+	    int yn = 0;
+	    keypad keypad1=new keypad();
+	    outerprinter.welcome();
+	    do
+	    {
+	        isvalid=aut.valid();
+	    }
+	    while(!isvalid);
+	    do 
+	    {
+	    	obj.myMenu();
+	        outerprinter.DisplayMessageln("do you want to exit:");
+	        outerprinter.DisplayMessageln("Choose 1(for yes) or 0(for no)");
+	        yn = keypad1.getInput();
+	    }
+	    while (yn==0);
+        if(yn==1)
+        {
+        	outerprinter.end();
+        }
+      }
+}
